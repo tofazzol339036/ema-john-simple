fc78294d849d5436fc4ee9930add3ea76dd20764
MD TOFAZZOL HOSSAIN <tofazzol339036@student.sust.edu>
2021-05-18 13:05:15 +0600

    ema john updated

---
+++ src/components/Product/Product.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './Product.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+
+const Product = (props) => {
+    const { img, name, seller, price, stock } = props.product;
+    return (
+        <div className="product">
+            <div>
+                <img src={img} alt="" />
+            </div>
+            <div className="product-address">
+                <h4 className="product-name">{name}</h4>
+                <br />
+                <p><small>by: {seller}</small></p>
+                <p>${price}</p>
+                <p><small>Only {stock} left in stock - Order soon</small></p>
+                <button 
+                    className="main-button"
+                    onClick = {() => props.handleAddProduct(props.product)}
+                >
+                    <FontAwesomeIcon className="icon" icon={faShoppingCart} />add to cart
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default Product;
---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import fakeData from '../../fakeData';
+import Product from '../Product/Product';
+import './Shop.css';
+
+const Shop = () => {
+    const first10 = fakeData.slice(0,10);
+    const [products, setProducts] = useState(first10);
+    const handleAddProduct = (product) => {
+        console.log('Product Added',product);
+    }
+    return (
+        <div className="shop-container">
+            <div className="product-container"> 
+                {
+                    products.map(pd => <Product
+                        handleAddProduct = {handleAddProduct}
+                         product={pd}>
+
+                         </Product> )
+                }
+            </div>
+            <div className="cart-container">
+                <h3>This is cart.</h3>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
